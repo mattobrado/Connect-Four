b0c77e0dc1125afd6ff7ba303ba55066799fed57
mattobrado <mcobradovic@gmail.com>
2022-05-23 17:10:11 -0500

    added comments to makeHtmlBoard()

--- connect4.js
+++ connect4.js
@@ -5,46 +5,59 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  board = []; // reset board in case makeBoard() is called multiple times
+
+  // fill board with HEIGHT number of arrays 
+  for (let row = 0; row < HEIGHT; row++){
+    board[row] = new Array(WIDTH); // internal arrays have length = WIDTH 
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.querySelector("#board"); // get HTML item w/ID "board"
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
+  // create table row element where players will click
+  const clickableRow = document.createElement("tr"); 
+  clickableRow.setAttribute("id", "clickable-row"); // set id to "clickable-row"
+  clickableRow.addEventListener("click", handleClick); // event listener for player's clicks
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
-    top.append(headCell);
+  // fill the clickable row with clickable cells
+  for (let columnIndex = 0; columnIndex < WIDTH; columnIndex++) {
+    const clickableCell = document.createElement("td"); // create td (cell) element
+    clickableCell.setAttribute("id", columnIndex); // set id of cell to column index
+    clickableRow.append(clickableCell); // append clickableCell to ClickableRow
   }
-  htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
-    const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
-      const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
-      row.append(cell);
+  // append clickable row to htmlBoard
+  htmlBoard.append(clickableRow);
+
+  // create html board
+
+  // iterate through rows
+  for (let rowIndex = 0; rowIndex < HEIGHT; rowIndex++) {
+    const row = document.createElement("tr");// create tablerow element 
+
+    //iterate throguh columns
+    for (let columnIndex = 0; columnIndex < WIDTH; columnIndex++) {
+      const cell = document.createElement("td"); // create cell
+      cell.setAttribute("id", `${rowIndex}-${columnIndex}`); // set id to "row-column"
+      row.append(cell); // append cell to row
     }
-    htmlBoard.append(row);
+
+    htmlBoard.append(row); // append row to htmlBoard
   }
 }
 
@@ -71,10 +84,10 @@ function endGame(msg) {
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
@@ -115,12 +128,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
